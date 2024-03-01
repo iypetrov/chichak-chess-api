b4eae9fced1a219cc96c5d582c6fb246ab00f4aa
I.P <ilia.yavorov.petrov@gmail.com>
2024-03-01 15:57:22 +0200

    init

---
+++ src/main/java/com/example/chichakchessapi/ChichakChessApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chichakchessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChichakChessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChichakChessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chichakchessapi/ChichakChessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chichakchessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChichakChessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
