4fb14efb32e9291db69b7d35a12f9315ab18e8e5
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-03-05 15:59:46 +0200

    add swagger + add basic security config + get version of api from .env

---
+++ README.md
@@ -0,0 +1,10 @@
+### HINT
+This is example content of `.env` file:
+```
+API_MAJOR_VERSION=1
+API_MINOR_VERSION=0
+API_PATCH_VERSION=0
+DB_URL=jdbc:mysql://localhost:3306/chessdb
+DB_USER=admin
+DB_PASSWORD=admin
+```
---
+++ src/main/java/com/example/chichakchessapi/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.example.chichakchessapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${app.client.host}")
+    private String clientHost;
+    @Value("${app.base-path}")
+    private String basePath;
+
+    @Bean
+    @Profile("local")
+    public SecurityFilterChain localFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    @Profile("!local")
+    public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors
+                        .configurationSource(request -> {
+                            CorsConfiguration corsConfiguration = new CorsConfiguration();
+                            corsConfiguration.setAllowedOrigins(List.of(
+                                    clientHost
+                            ));
+                            corsConfiguration.setAllowedMethods(List.of(
+                                    HttpMethod.GET.name(),
+                                    HttpMethod.POST.name(),
+                                    HttpMethod.PUT.name(),
+                                    HttpMethod.PATCH.name(),
+                                    HttpMethod.DELETE.name()
+                            ));
+                            corsConfiguration.setAllowedHeaders(List.of(
+                                    "*"
+                            ));
+                            corsConfiguration.setAllowCredentials(true);
+                            return corsConfiguration;
+                        })
+                )
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers(basePath + "/**")
+                        .ignoringRequestMatchers("/health-check")
+                        .ignoringRequestMatchers("/swagger-ui/**")
+                        .ignoringRequestMatchers("/v3/api-docs/**")
+                )
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(basePath + "/**").permitAll()
+                        .requestMatchers("/health-check").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+}
