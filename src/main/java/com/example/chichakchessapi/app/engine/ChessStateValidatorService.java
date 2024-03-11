c161355e06aaa2e1bfad0a22f34706f1f49dfb9c
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-03-11 16:51:48 +0200

    init chess engine

--- src/main/java/com/example/chichakchessapi/app/engine/ChessStateValidatorService.java
+++ src/main/java/com/example/chichakchessapi/app/engine/ChessStateValidatorService.java
@@ -1,12 +1,22 @@
 package com.example.chichakchessapi.app.engine;
 
+import com.example.chichakchessapi.app.gamestates.models.GameStateModel;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class ChessStateValidatorService {
     public static final String INIT_BOARD_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 
-    private ChessStateValidatorService() {
-        throw new IllegalStateException("Will be implemented in the next PR");
+    private final List<GameStateModel> gameStatesToPersistQueue;
+    private final Map<String, GameStateModel> gameStateIdsToLatestGameStates;
+
+    public ChessStateValidatorService() {
+        this.gameStatesToPersistQueue = new ArrayList<>();
+        this.gameStateIdsToLatestGameStates = new ConcurrentHashMap<>();
     }
 }
